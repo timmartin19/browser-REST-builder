9c104e752803b0f71e3980a3e42a4d3fdb0e3264
timmartin19 <tim.martin@vertical-knowledge.com>
2015-07-13 20:37:52 -0400

    Setup completed.

---
+++ setup.py
@@ -0,0 +1,47 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+
+setup(
+    author='Tim Martin',
+    author_email='tim.martin@vertical-knowledge.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    description='A browser based client for building and visualizing REST API\'s with ripozo.',
+    include_package_data=True,
+    install_requires=[
+        'Flask-SQLAlchemy',
+        'flask-ripozo',
+        'ripozo-sqlalchemy',
+    ],
+    keywords='REST HATEOAS Hypermedia RESTful web API browser visualization',
+    name='browser-rest-builder',
+    packages=find_packages(exclude=['rest_builder_tests', 'rest_builder_tests.*']),
+    test_requires=[
+        'mock',
+        'tox',
+        'unittest2',
+    ],
+    test_suite='rest_builder_tests',
+    version=version,
+)
+
